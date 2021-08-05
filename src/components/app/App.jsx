f3f411ad9e0a6b0bd542bf090d37b02c12461176
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-08-05 11:04:44 -0700

    added router to app

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Switch,  Route } from 'react-router-dom';
+import Header from './Header';
+import CharacterList from '../characters/CharacterList';
+import CharacterDetails from '../details/CharacterDetails';
+
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Switch>
+      <Route exact path="/" component={CharacterList} />
+      <Route exact path="/" component={CharacterDetails} />
+    </Switch>
+  );
 }
