80023de286077f806b656926db4472821086b0c2
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-08-05 11:41:13 -0700

    added character component

---
+++ src/components/characters/Character.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const Character = ({ id, name, status, image }) => {
+  return (
+    <Link to={id}>
+      <h2>{name}</h2>
+      <img src={image} alt={name} />
+      <p>{status}</p>
+    </Link>
+  );
+};
+  // <figure>
+  //   <img src={image} alt={name} />
+  //   <figcaption>
+  //     <h2>{name}</h2>
+  //     <p>{status}</p>
+  //   </figcaption>
+  // </figure>
+// );
+
+Character.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+};
+
+export default Character;
